856f6af3085bfad6cfcbdc183116a217552c6f9b
ChapelgateRobotics <chapelgategobotics@gmail.com>
2020-09-23 18:19:30 -0400

    Extends opmode

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Telop.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created Kirk Frankfurth on 9/23/20 in SkyStone-master.
+ * <p>
+ * Copyright (c) ©2020 Kirk Frankfurth
+ * Copyright (c) ©2020 Mentor of Team VECTOR (5233) Kirk
+ * Resource: https:/gitlab.com/robotics/ftc_app
+ * Contact: kfrankfurth@chapelgateacademy.org, vector5233@gmail.com
+ */
+public class Telop extends OpMode
+{
+    @Override
+    public void init()
+    {
+
+    }
+
+    @Override
+    public void loop()
+    {
+
+    }
+}
